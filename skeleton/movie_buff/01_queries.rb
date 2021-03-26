6fed5f7855ae1c5186708d913151936d86fa55ab
aturi <angelaturi99@gmail.com>
2021-03-26 15:24:54 -0400

    Add biggest_cast, directed_by_one_of, movie_names_before_1940

--- skeleton/movie_buff/01_queries.rb
+++ skeleton/movie_buff/01_queries.rb
@@ -42,6 +42,13 @@ def biggest_cast
   # Find the id and title of the 3 movies with the
   # largest casts (i.e most actors)
 
+  Movie
+    .select(:id, :title)
+    .joins(:actors)
+    .group('movies.id')
+    .order('COUNT(actors.id) DESC')
+    .limit(3)
+
 end
 
 def directed_by_one_of(them)
@@ -57,6 +64,11 @@ def directed_by_one_of(them)
   #
   # Find the id and title of all the movies directed by one of 'them'.
 
+  Movie
+    .select(:id, :title)
+    .joins(:director)
+    .where(actors: {name: them})
+
 end
 
 def movie_names_before_1940
@@ -70,5 +82,9 @@ def movie_names_before_1940
   # improve performace for larger queries.
   #
   # Use pluck to find the title of all movies made before 1940.
+
+  Movie
+    .where('yr < 1940')
+    .pluck(:title)
 
 end
